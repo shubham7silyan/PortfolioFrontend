4e0dff11cdff5276f3ce49c0211b9e486193dd97
Shubham Silyan <shubham7silyan@gmail.com>
2025-09-05 03:54:15 +0530

    fix:  Railway npm ci failure

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import '../index.css'
 import pi1 from '../images/divider.png'
 import pi2 from '../images/about_me_pic2.jpg'
